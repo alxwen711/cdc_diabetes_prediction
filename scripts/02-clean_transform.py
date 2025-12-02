b09ee3001b3f2e132de3747c0823b737a3515659
Michael Eirikson <134233316+Michael9381@users.noreply.github.com>
2025-12-01 21:27:56 -0800

    created scripts folder

---
+++ scripts/02-clean_transform.py
@@ -0,0 +1,19 @@
+"""Clean and transform raw data.
+
+This script will load the raw data from /data/raw/diabetes_raw_features.csv
+and /data/raw/diabetes_raw_features.csv
+"""
+
+# import libraries/packages
+
+# parse/define command line arguments here
+
+# code for other functions
+
+# define main function
+def main():
+    # code for "guts" of script goes here
+
+# call main function
+if __name__ == "__main__":
+    main() # pass any command line args to main here
