40c742c5e746d8079d01ff63346908895551a6de
alxwen711 <alxwen711@gmail.com>
2025-12-01 20:16:44 -0800

    working obtaining and saving of raw data

--- scripts/01-download_extract.py
+++ scripts/01-download_extract.py
@@ -2,39 +2,25 @@ import pandas as pd
 import pandera.pandas as pa
 import os
 from ucimlrepo import fetch_ucirepo 
+import click
 
 
+def obtain_raw_data():
+    """
 
-def raw_data():
-    """short_description
-    
-    longer_description
-    
-    Notes
-    -----
-    
-    Parameters
-    ----------
-    a : int
-        description
-    b : int, optional (default = 0)
-        description
+    Obtains the raw CDC diabetes data for the analysis via the UCI ML Repository.
     
     Returns
     -------
-    int
-        description
+    pd.Dataframe, pd.Dataframe
+        Two data frames, the first corresponding to the [X] features of the data set,
+        and the second corresponding to the [y] target labels of the data set.
     
     Examples
     --------
-    >>> snake_case(a, b)
-    output
-    
-    Raises
-    --------
-    SomeError
-        when some error
-    
+    >>> obtain_raw_data()
+    pd.Dataframe, pd.Dataframe
+        
     """
     # fetch dataset 
     cdc_diabetes_health_indicators = fetch_ucirepo(id=891) 
@@ -125,51 +111,66 @@ def validate_x_data(a: int, b: int = 0) -> int:
 
     schema.validate(X, lazy = True)
 
+#@click.command()
+#@click.option("--filepath", type = str, default = "../data/raw", help = "Directory location to write the combined DataFrame in csv format.")
+#@click.option("--filename", type = str, default = "diabetes_raw.csv", help = "Name of the file to write the DataFrame to.")
+#@click.option("--label", type = str, default = "diabetes", help = "Name of the target value column in the combined DataFrame.")
+def save_raw_data(X: pd.DataFrame, y: pd.DataFrame, filepath: str = "data/raw", filename: str = "diabetes_raw.csv", label: str = "diabetes"):
+    """
 
-def save_raw_data(a: int, b: int = 0) -> int:
-    """short_description
-    
-    longer_description
-    
-    Notes
-    -----
+    Writes the raw analysis dataframes to a single file. If the
+    file path does not exist they will be created by this function.
     
     Parameters
     ----------
-    a : int
-        description
-    b : int, optional (default = 0)
-        description
-    
-    Returns
-    -------
-    int
-        description
+    X : pd.DataFrame
+        DataFrame corresponding to the features of the data set.
+    y : pd.DataFrame
+        DataFrame corresponding to the labels of the data set.
+    filepath: str, default = "data/raw"
+        Directory location to write the combined DataFrame in csv format.
+    filename: str, default = "diabetes_raw.csv"
+        Name of the file to write the DataFrame to.
+    label: str, default = "diabetes"
+        Name of the target value column in the combined DataFrame.
     
     Examples
     --------
-    >>> snake_case(a, b)
-    output
+    >>> X,y = obtain_raw_data()
+    >>> save_raw_data(X, y, "data/raw", "diabetes_raw.csv", "diabetes")
     
     Raises
     --------
-    SomeError
-        when some error
-    
+    TypeError
+        Either X or y is not a valid DataFrame.
+    ValueError
+        Attempt to combine the DataFrames failed, the full error is additionally outputted.
     """
+
+    # Try creating the combined DataFrame
+    if not isinstance(X, pd.DataFrame): raise TypeError("X object obtained is not a Pandas Dataframe.")
+    if not isinstance(y, pd.DataFrame): raise TypeError("y object obtained is not a Pandas Dataframe.")
+
+    data = X
+
+    try:
+        data[label] = y
+    except Exception as e:
+        raise ValueError(f"Attempt to combine the X and y Dataframes failed due to the following: {e}")
+
     # check if raw folder exists
-    raw_data_path = "../data/raw"
+    
+    if not os.path.exists(filepath):
+        os.makedirs(filepath)
 
-    if not os.path.exists(raw_data_path):
-        os.makedirs(raw_data_path)
+    # Save Raw Data
+    data.to_csv(os.path.join(filepath,filename))
 
-    ## Save Raw Data
-    X.to_csv("../data/raw/diabetes_raw_features.csv")
-    y.to_csv("../data/raw/diabetes_raw_targets.csv")
-
+    
 
 def main():
-    return
+    X,y = obtain_raw_data()
+    save_raw_data(X,y)
 
 if __name__ == "__main__":
     main()
