5de04924736ba54b6e58b421dc56f72843878c10
alxwen711 <alxwen711@gmail.com>
2025-12-02 23:23:32 -0800

    rework command options, eda_describe function draft, note script structure plan

--- scripts/04-EDA.py
+++ scripts/04-EDA.py
@@ -1,104 +1,49 @@
 import click
-
+import pandas as pd
 # import script 03-split_preprocess_data.py to obtain training data
 preprocess_name = "03-split_preprocess_data"
 preprocess = __import__(preprocess_name)
 
-def eda_head():
-    """short_description
-    
-    longer_description
-    
-    Notes
-    -----
+def eda_describe(df):
+    """Macro function to output the head, tail, and description (df.describe) of a DataFrame for EDA purposes
     
     Parameters
     ----------
-    a : int
-        description
-    b : int, optional (default = 0)
-        description
-    
-    Returns
-    -------
-    int
-        description
+    df : pd.DataFrame
+        Assummed training data
     
     Examples
     --------
-    >>> snake_case(a, b)
-    output
+    >>> d = {'col1': [1, 2], 'col2': [3, 4]}
+    >>> df = pd.DataFrame(data=d)
+    >>> eda_describe(df)
     
-    Raises
-    --------
-    SomeError
-        when some error
-    
-    """
+       col1  col2
+    0     1     3
+    1     2     4
 
-def eda_tail():
-    """short_description
-    
-    longer_description
-    
-    Notes
-    -----
-    
-    Parameters
-    ----------
-    a : int
-        description
-    b : int, optional (default = 0)
-        description
-    
-    Returns
-    -------
-    int
-        description
-    
-    Examples
-    --------
-    >>> snake_case(a, b)
-    output
-    
-    Raises
-    --------
-    SomeError
-        when some error
-    
-    """
 
-def eda_describe():
-    """short_description
-    
-    longer_description
-    
-    Notes
-    -----
-    
-    Parameters
-    ----------
-    a : int
-        description
-    b : int, optional (default = 0)
-        description
-    
-    Returns
-    -------
-    int
-        description
-    
-    Examples
-    --------
-    >>> snake_case(a, b)
-    output
-    
-    Raises
-    --------
-    SomeError
-        when some error
-    
+       col1  col2
+    0     1     3
+    1     2     4
+
+
+               col1      col2
+    count  2.000000  2.000000
+    mean   1.500000  3.500000
+    std    0.707107  0.707107
+    min    1.000000  3.000000
+    25%    1.250000  3.250000
+    50%    1.500000  3.500000
+    75%    1.750000  3.750000
+    max    2.000000  4.000000
     """
+    print(df.head())
+    print("\n")
+    print(df.tail())
+    print("\n")
+    print(df.describe())
+    
 
 def eda_count():
     """short_description
@@ -269,29 +214,25 @@ def save_figure(plot, path: str):
     "-c", 
     type = str, 
     default = "count", 
-    help = "Run a specific EDA command from the list of EDA tasks done in order: \n ['head', 'tail', 'describe', 'count', 'histogram', 'boxplot', 'correlation']"
+    help = "Run a specific EDA command from the list of EDA tasks done in order: \n ['describe', 'count', 'histogram', 'boxplot', 'correlation', 'saveallcharts']"
 )
 @click.option(
-    "--save",
-    "-s",
+    "--path",
+    "-p",
     type = str,
     default = None,
-    help = "Specify a specific directory and filename to save a generated image to if the EDA command is among the following: \n ['count', 'boxplot', 'histogram', 'correlation']"
+    help = "Specify a specific directory to save a generated image to if the EDA command is among the following: \n ['count', 'boxplot', 'histogram', 'correlation', 'saveallcharts']"
 )
-def run_eda_function(command: str, save = None):
+def run_eda_function(command: str, path = None):
     plot = None
     X_train,y_train = preprocess.load_and_split_file("data/clean/diabetes_clean_train.csv","train")
-    print(X_train.head())
+    train_df = X_train
+    train_df["diabetes"] = y_train
+    
     match command:
-        case "head":
-            click.echo("Printing first few rows of training data...")
-            #eda_head(train_df)
-        case "tail":
-            click.echo("Printing last few rows of training data...")
-            #eda_tail(train_df)
         case "describe":
             click.echo("Printing description of training data...")
-
+            eda_describe(train_df)
         case "count":
             click.echo("Creating basic bar plot for count of target labels...")
         case "histogram":
@@ -300,17 +241,31 @@ def run_eda_function(command: str, save = None):
             click.echo("Creating boxplots of all non-binary features...")
         case "correlation":
             click.echo("Creating feature-feature correlation plot on training data...")
+        case "saveallcharts":
+            click.echo(f"Creating and saving ALL EDA charts to directory {path}...")
         case _:
-            click.echo("Command is not recognized from the options ['head', 'tail', 'describe', 'count', 'boxplot', 'histogram', 'correlation'], no action performed")
+            click.echo("Command is not recognized from the options ['describe', 'count', 'boxplot', 'histogram', 'correlation', 'saveallcharts'], no action performed")
             return
     click.echo(f"Command {command} compeleted as expected.")
-    if save != None and command in ["count", "boxplot", "histogram", "correlation"]:
-        click.echo(f"Saving plot to {save}...")
-        save_figure(plot,save)
-        click.echo(f"Plot has been saved to {save}.")
+    if path != None and command in ["count", "boxplot", "histogram", "correlation"]:
+        click.echo(f"Saving plot to {path}...")
+        save_figure(plot,path,command)
+        click.echo(f"Plot has been saved to {path}.")
+
+"""
+TODO:
+
+Currently run EDA function is pulling up the X_train and y_train for each individual function, either:
+- rework the interface to load data first and then run multiple commands at a time (see next points for changes)
+- add additional command to make all of the plots at once
+- combine head/tail/describe blocks into a single function (they appear sequentially in the ipynb already)
+
+- assuming above template is accurate, then use ipynb code to fill in the gaps
+"""
+
 
 if __name__ == "__main__":
     run_eda_function()
-
+    
 
 
