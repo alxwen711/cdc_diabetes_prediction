6ad33a02a99f8d37e664b3d30e9ab4fbf6bcad28
alxwen711 <alxwen711@gmail.com>
2025-12-08 16:53:45 -0800

    wip generate_label function setup

---
+++ src/generate_label.py
@@ -0,0 +1,24 @@
+import pandas as pd
+
+def generate_label(row: pd.Series) -> str:
+    """Helper function to create the x-axis labels for eda_binary.
+    
+    Parameters
+    ----------
+    row : pd.Series
+        A row of a pandas Dataframe.
+
+    Returns
+    -------
+    str
+        str type label used in the binary bar plots.
+    """
+    return f"f = {row['feature_value']}, d = {row['diabetes']}"
+
+
+# pytz and jsonschema and toolz needed to resolve the imports
+
+if __name__ == "__main__":
+    df = pd.DataFrame({'feature_value': [1,2], 'diabetes': [3,4]})
+    df["label"] = df.apply(generate_label, axis = 1)
+    print(df)
