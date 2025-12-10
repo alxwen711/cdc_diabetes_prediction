e0f32a60ce58620059f5115c7fcc6561fcb835c8
alxwen711 <alxwen711@gmail.com>
2025-12-10 14:06:03 -0800

    add extra tests for generate_label (error tests)

--- tests/generate_label_test.py
+++ tests/generate_label_test.py
@@ -17,6 +17,7 @@ def test_simple():
     simple_df["label"] = simple_df.apply(generate_label, axis = 1)
     pd.testing.assert_frame_equal(simple_df,simple_expected_df)
 
+
 # test arguments
 complex_df = pd.DataFrame({"a": [True], "b": ["snth"]})
 complex_expected_df = pd.DataFrame({"a": [True], "b": ["snth"], "c": ["e = True, f = snth"]})
@@ -32,10 +33,28 @@ def test_complex():
 
 
 # edge case for one column
-def test_mono_column():
-    pass
+mono_df = pd.DataFrame({"number": [1,2,3,4,5]})
+mono_expected_df = pd.DataFrame({"number": [1,2,3,4,5], 
+                                 "mono": ["f = 1, d = 1", "f = 2, d = 2", "f = 3, d = 3", "f = 4, d = 4", "f = 5, d = 5"]})
 
-# column does not exist in dataframe error
+def test_mono_column():
+    mono_df["mono"] = mono_df.apply(generate_label, feature_name = "number", value_name = "number", axis = 1)
+    pd.testing.assert_frame_equal(mono_df, mono_expected_df)
+
+
+# column does not exist in dataframe error 
+not_found_df = pd.DataFrame({"nope": ["NO","non","nada","0","nothing"]}) 
+
 def test_column_not_found():
-    pass
+    with pytest.raises(KeyError):
+        not_found_df["answer"] = not_found_df.apply(generate_label, feature_name = "yes", axis = 1)
+
+
+# dataframe has no columns
+
+empty_df = pd.DataFrame()
+
+def test_empty_dataframe():
+    with pytest.raises(ValueError):
+        empty_df["aaaaa"] = empty_df.apply(generate_label, axis = 1)    
 
